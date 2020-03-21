a5428be1684b5575872aebdc38a0eef3de3818e0
Kevin Meredith <kevin.m.meredith@gmail.com>
2020-03-21 00:19:32 -0400

    Adjust spec.

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -7,6 +7,7 @@ import doobie.implicits._
 import doobie.util.ExecutionContexts
 import doobie.util.query.Query0
 import doobie.util.transactor.Transactor
+import scala.concurrent.duration._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
@@ -21,10 +22,29 @@ object Main extends IOApp {
 
     val query: ConnectionIO[Unit] = sleep100Seconds.unique
 
-    query.transact(xa).as(ExitCode.Success)
+    val postgresSleep100Secons: IO[Unit] =
+      query
+        .transact(xa)
+
+    val race: IO[Unit] =
+      IO.race(sleep1Second, postgresSleep100Secons)
+      .flatMap {
+        case e: Either[Unit, Unit] => IO(println(">>> winner:" + e.toString))
+      }
+
+    for {
+      _ <- IO(println(java.time.Instant.now))
+      _ <- race
+      _ <- IO(println(java.time.Instant.now))
+    } yield ExitCode.Success
+
+
   }
 
+  private val sleep1Second: IO[Unit] =
+    IO.sleep(1.second)
+
   private val sleep100Seconds: Query0[Unit] =
-    sql"""SELECT pg_sleep(100)""".query[Unit]
+    sql"""SELECT pg_sleep(10)""".query[Unit]
 
 }
